f8bb8fc7ef8a7c88558b81adc2998080f38ab675
Fyroo <chaouimoutaa@gmail.com>
2023-03-05 20:54:45 +0100

    merged branch

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,4 +1,7 @@
 
+import { tokens } from "../theme";
+
+
 export const mockDataMetro = [
   {
     id: 1,
@@ -91,4 +94,154 @@ export const mockDataMetro = [
     Model: "ME-3800",
     access: "admin",
   },
+
+  {
+    id: 14,
+    name: "GHAZELA",
+    IP: "10.141.1.7",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 15 ,
+    name: "ENNASR I",
+    IP: "10.141.3.1",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 16,
+    name: "JARDIN EL MANZAH II",
+    IP: "10.141.3.2",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 17,
+    name: "EJARDIN MANZAH I",
+    IP: "10.141.3.3",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 18,
+    name: "ENNASR II",
+    IP: "10.141.2.4",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 19,
+    name: "ENNASR II",
+    IP: "10.141.2.9",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 20,
+    name: "EL MANAR",
+    IP: "10.141.2.3",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 21,
+    name: "MANAR",
+    IP: "10.141.2.1",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 22,
+    name: "MANAR 2",
+    IP: "10.141.2.6",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 23,
+    name: "EL MANZAH 6",
+    IP: "10.141.2.5",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 24,
+    name: "MANZAH",
+    IP: "10.141.2.8",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 25,
+    name: "AEROPORT TUNIS CARTHAGE",
+    IP: "10.141.4.2",
+    Model: "ASR 920",
+    access: "admin",
+  },
+  {
+    id: 26,
+    name: "CENTRE DE TRI",
+    IP: "10.141.4.8",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 27,
+    name: "SIDI FRADJ",
+    IP: "10.141.4.3",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 28,
+    name: "SIDI FRADJ",
+    IP: "10.141.4.6",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 29,
+    name: "SIDI FRADJ",
+    IP: "10.141.4.1",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 30,
+    name: "TAIEB MHIRI",
+    IP: "10.141.6.3",
+    Model: "ASR 9030",
+    access: "admin",
+  },
+  {
+    id: 31,
+    name: "JEMAA RAOUDHA",
+    IP: "10.141.6.1",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 32,
+    name: "AOUINA",
+    IP: "10.141.4.7",
+    Model: "ME-3800",
+    access: "admin",
+  },
+  {
+    id: 33,
+    name: "SOUKRA",
+    IP: "10.141.4.9",
+    Model: "ASR 903",
+    access: "admin",
+  },
+  {
+    id: 34,
+    name: "BORJ LOUZIR",
+    IP: "10.141.6.2",
+    Model: "ASR 903",
+    access: "admin",
+  },
+ 
 ]
+
